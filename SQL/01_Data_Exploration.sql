3e33f5a321c35de527ab96280f83824860256f9a
diyasharma21 <diyaasharma2103@gmail.com>
2026-08-01 08:32:02 +0530

    Refactor SQL exploration queries for efficiency
    Replaced individual SELECT statements with DESCRIBE for table structure and added COUNT queries for customers, accounts, loans, and transactions.

--- SQL/01_Data_Exploration.sql
+++ SQL/01_Data_Exploration.sql
@@ -2,30 +2,23 @@ USE rbip;
 
 SHOW TABLES;
 
-SELECT *
-FROM customers
-LIMIT 10;
+DESCRIBE customers;
+DESCRIBE accounts;
+DESCRIBE cards;
+DESCRIBE loans;
+DESCRIBE merchants;
+DESCRIBE branches;
+DESCRIBE transactions;
 
-SELECT *
-FROM accounts
-LIMIT 10;
+SELECT * FROM customers LIMIT 10;
+SELECT * FROM accounts LIMIT 10;
+SELECT * FROM cards LIMIT 10;
+SELECT * FROM loans LIMIT 10;
+SELECT * FROM merchants LIMIT 10;
+SELECT * FROM branches LIMIT 10;
+SELECT * FROM transactions LIMIT 10;
 
-SELECT *
-FROM cards
-LIMIT 10;
-
-SELECT *
-FROM loans
-LIMIT 10;
-
-SELECT *
-FROM merchants
-LIMIT 10;
-
-SELECT *
-FROM branches
-LIMIT 10;
-
-SELECT *
-FROM transactions
-LIMIT 10;
+SELECT COUNT(*) FROM customers;
+SELECT COUNT(*) FROM accounts;
+SELECT COUNT(*) FROM loans;
+SELECT COUNT(*) FROM transactions;
